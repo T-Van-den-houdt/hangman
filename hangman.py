5df27e1b69abcddf15dcbd3abb24546e453fb973
Thomas <thomas.vandenhoudt1@student.ucll.be>
2024-03-22 15:04:08 +0100

    changed the way letters are found

--- hangman.py
+++ hangman.py
@@ -1,67 +1,136 @@
 import os
 import time
 
-def find_letters(letter, word):
-    for i in range(len(word)):
-        if word[i] == letter:
-            return i
+HANGMANPICS = ['''
+      
+      
+      
+    
+      
+      
+=========''', '''
+      +
+      |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
 
 def won(word):
     os.system("clear")
     print("Congrats, you've won!")
     print(f"The word was {word}")
+    print('''            
+             ___________
+            '._==_==_=_.'
+            .-\:      /-.
+           | (|:.     |) |
+            '-|:.     |-'
+              \::.    /
+               '::. .'
+                 ) (
+               _.' '._
+              `"""""""`
+              ''')
 
 def lose(word):
     os.system("clear")
     print("You're so bad, you lost!")
     print(f"The word was {word}")
-
+    
 def main():
     os.system('clear')
     word = ""
-    lifes = 10
+    lifes = 8
     win = False
 
     print("Welcome to Hangman!")
     print("Player 1, please enter the word you want to be guessed!")
     
-    word = input()
-    anon_word = "_" * len(word)
-
+    word = input().lower()
+    anon_word = "-" * len(word) 
     guessed_letters = []
+    hangmanStage = 0
 
     while (win is False) and (lifes > 0):
         os.system('clear')
-        
+
         print(f"Not in the word: {guessed_letters}")
-        print(f"You have {lifes} lifes left.")
+        print(f"You have {lifes} chances left.")
+        print(HANGMANPICS[hangmanStage])
         print(f"{anon_word}")
-        print("What is your guess:")
-        print("-------------------------------------------------------")
+        print("\nEnter your guess\n")
+        print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
 
-        guess = input()
-        amount = 0
+        guess = ""
+        while len(guess) != 1:
+            guess = input("Please guess a letter:").lower()
+        
+        if guess not in word:
+            lifes -= 1
+            hangmanStage += 1 
+            if guess not in guessed_letters:
+                guessed_letters.append(guess)
+        else:
+            list_anon = list(anon_word)
+            for i in range(len(list(word))):
+                if list(word)[i] == guess:
+                    list_anon[i] = guess
+            anon_word = "".join(list_anon)
 
-        if guess == word:
+        if anon_word == word:
             won(word)
             win = True
-        
-        for l in guess:
-            if l in word:
-                i = find_letters(l, word)
-               
-                anon_word = list(anon_word)
-                anon_word[i] = l
-                anon_word = ''.join(anon_word)
-
-            else:
-                amount += 1
-
-                if amount == len(guess):
-                    lifes -= 1
-
-                if l not in guessed_letters:
-                    guessed_letters.append(l)
     
     if win == False:
         lose(word)
